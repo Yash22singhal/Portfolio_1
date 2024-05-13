00f692c01050dcd3252c46b61783c843fda588c3
YASH <ysinghal2003@gmail.com>
2024-05-13 08:22:41 +0530

    added js for typed animation

---
+++ script.js
@@ -0,0 +1,8 @@
+// Typing Effect
+let typed = new Typed('.auto-text', {
+    strings: ['Web Developer', 'Web Designer', 'Software Engineer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true
+});
